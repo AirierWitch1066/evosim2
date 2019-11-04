4bbe723e39e727086f7c03aa80cb60afd8b0f21f
Gunnar Noordhoek <gunnar.noordhoek@nelnet.net>
2019-11-04 07:57:45 -0600

    tests require 100% coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,35 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  //preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testMatch: ["**/unit/**/*.spec.ts"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "test/tsconfig.json"
+    }
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputName: "./unit.junit.xml",
+        outputDirectory: "./test-reports",
+        suiteNameTemplate: "{filename}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}"
+      }
+    ]
+  ],
+  collectCoverageFrom: ["**/src/**/*.ts", "!**src/interfaces/**/*.ts"],
+  coverageThreshold: {
+    global: {
+      functions: 100,
+      branches: 100,
+      lines: 100
+    }
+  }
 };
